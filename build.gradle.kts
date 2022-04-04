1127514f1a42b89d9eb8e548a09ddba0ebde2902
Cody Worsnop <cworsnop@figure.com>
2022-04-04 10:10:32 -0700

    build updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,10 @@ plugins {
 
 semver {
     tagPrefix("v")
-    initialVersion("0.2.4")
+    initialVersion("0.2.6")
+    findProperty("semver.overrideVersion")?.toString()?.let { overrideVersion(it) }
+    val semVerModifier = findProperty("semver.modifier")?.toString()?.let { buildVersionModifier(it) } ?: { nextPatch() }
+    versionModifier(semVerModifier)
 }
 
 /**
