d03e14be19c682347ab20f5eabac70eb483ed2b3
Cody Worsnop <cworsnop@figure.com>
2022-05-06 11:11:02 -0700

    fixing some logic error

--- lib/src/main/kotlin/io/provenance/core/Originator.kt
+++ lib/src/main/kotlin/io/provenance/core/Originator.kt
@@ -1,5 +1,6 @@
 package io.provenance.core
 
+import io.provenance.scope.encryption.util.toJavaPrivateKey
 import io.provenance.scope.encryption.util.toJavaPublicKey
 import java.security.Key
 
@@ -8,16 +9,14 @@ class Originator(
 ) {
     private val keyMap = mutableMapOf<KeyType, Key>()
 
-    fun signingPublicKey() = getKey(KeyType.SIGNING_PUBLIC_KEY)
-    fun signingPrivateKey() = getKey(KeyType.SIGNING_PRIVATE_KEY)
-    fun encryptionPublicKey() = getKey(KeyType.ENCRYPTION_PUBLIC_KEY)
-    fun encryptionPrivateKey() = getKey(KeyType.ENCRYPTION_PRIVATE_KEY)
-    fun authorizationPrivateKey() = getKey(KeyType.AUTH_PRIVATE_KEY)
-    fun authorizationPublicKey() = getKey(KeyType.AUTH_PUBLIC_KEY)
+    fun signingPublicKey() = getKey(KeyType.SIGNING_PUBLIC_KEY) { it.toJavaPublicKey() }
+    fun signingPrivateKey() = getKey(KeyType.SIGNING_PRIVATE_KEY) { it.toJavaPrivateKey() }
+    fun encryptionPublicKey() = getKey(KeyType.ENCRYPTION_PUBLIC_KEY) { it.toJavaPublicKey() }
+    fun encryptionPrivateKey() = getKey(KeyType.ENCRYPTION_PRIVATE_KEY) { it.toJavaPrivateKey() }
+    fun authorizationPrivateKey() = getKey(KeyType.AUTH_PRIVATE_KEY) { it.toJavaPrivateKey() }
+    fun authorizationPublicKey() = getKey(KeyType.AUTH_PUBLIC_KEY) { it.toJavaPublicKey() }
 
-    private fun getKey(keyType: KeyType) = keyMap.getOrDefault(keyType, null)?.let {
-        keys[KeyType.SIGNING_PUBLIC_KEY].toString().toJavaPublicKey()
-    }?.also {
-        keyMap[keyType] = it
+    private fun getKey(keyType: KeyType, transform: (String) -> Key) = keyMap.getOrPut(keyType) {
+        keys[keyType].toString().let(transform)
     }
 }
