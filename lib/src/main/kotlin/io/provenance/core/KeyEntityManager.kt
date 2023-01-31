d555168f48deffe3ddfec3504c734fa2a5225a7f
Cody Worsnop <cworsnop@figure.com>
2023-01-31 15:28:09 -0800

    cleanup

--- lib/src/main/kotlin/io/provenance/core/KeyEntityManager.kt
+++ lib/src/main/kotlin/io/provenance/core/KeyEntityManager.kt
@@ -6,13 +6,13 @@ class KeyEntityManager {
     private val entities: MutableMap<String, KeyEntity> = mutableMapOf()
     private val plugins: MutableSet<Plugin<*>> = mutableSetOf()
 
-    fun <T : PluginConfig> register(plugin: Plugin<T>) {
+    fun <T: PluginConfig> register(plugin: Plugin<T>) {
         if (plugins.filterIsInstance<Plugin<T>>().isEmpty()) {
             plugins.add(plugin)
         }
     }
 
-    fun <T : PluginConfig> get(entity: String, config: T): KeyEntity {
+    fun <T: PluginConfig> get(entity: String, config: T): KeyEntity {
         if (entity !in entities) {
             entities[entity] = plugins.filterIsInstance<Plugin<T>>().singleOrNull()?.fetch(entity, config)
                 ?: throw IllegalStateException("$entity did not have exactly one supported plugin.")
